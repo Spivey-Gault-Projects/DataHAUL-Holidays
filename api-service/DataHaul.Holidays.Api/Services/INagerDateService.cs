e0aba2d701a742f4c3258747dff54b269ae3fc26
Spivey Gault <142345823+unconventional-spivey@users.noreply.github.com>
2025-05-28 20:05:04 -0500

    Revert "Initial scaffolding of api-service"

--- api-service/DataHaul.Holidays.Api/Services/INagerDateService.cs
+++
@@ -1,9 +0,0 @@
-using DataHaul.Holidays.Api.Models;
-
-namespace DataHaul.Holidays.Api.Services
-{
-	public interface INagerDateService
-	{
-		Task<IEnumerable<Holiday>> GetPublicHolidaysAsync(int year, string countryCode);
-	}
-}
